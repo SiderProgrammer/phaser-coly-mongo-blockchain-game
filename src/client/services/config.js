000e7d43ef425c30b57c980bcabbdf0dd5e8c892
Sider <patryk.murawski.developer@gmail.com>
2022-07-07 17:03:48 +0200

    update

--- src/client/services/config.js
+++ src/client/services/config.js
@@ -1,5 +1,10 @@
-// const host = window.document.location.host.replace(/:.*/, '');
-// const port = process.env.NODE_ENV !== 'production' ? Constants.WS_PORT : window.location.port;
-// const url = `${window.location.protocol.replace('http', 'ws')}//${host}${port ? `:${port}` : ''}`;
 import { SERVER_PORT } from "../../shared/config";
-export const SERVER_URL = `http://localhost:${SERVER_PORT}`;
+
+const SERVER_URL = `${location.protocol}//${location.hostname}`;
+
+if (location.protocol !== "https:") {
+  // ? check if not hosted
+  SERVER_URL += `:${SERVER_PORT}`;
+}
+
+export { SERVER_URL };
